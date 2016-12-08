c8db6c1fcb99a3261aaeaeccb1d542ecbabcb6a7
fabianonunes <fabiano@nunes.me>
2016-12-08 07:37:19 -0200

    entradas js

--- src/js/main.js
+++ src/js/main.js
@@ -1,31 +1,26 @@
-/* global $ */
-(function () {
-  'use strict'
+var $ = require('jquery')
 
-  // DATEPICKER
-  $('document').ready(function () {
-    var $datePicker = $('#datepicker')
-    var $myHiddenInput = $('#my_hidden_input')
+// DATEPICKER
+$('document').ready(function () {
+  var $datePicker = $('#datepicker')
+  var $myHiddenInput = $('#my_hidden_input')
 
-    $datePicker.datepicker()
-    $datePicker.on('changeDate', function () {
-      $myHiddenInput.val(
-        $datePicker.datepicker('getFormattedDate')
-      )
-    })
+  $datePicker.datepicker()
+  $datePicker.on('changeDate', function () {
+    $myHiddenInput.val(
+      $datePicker.datepicker('getFormattedDate')
+    )
   })
+})
 
-  // COLAPSE SIDEBAR OPEN
+// BOTÕES SIDEBAR
+$('#main-menu-toggle').on('click', function () {
+  $('.content-sf, .navbar').toggleClass('active')
+  $('.sidebar-sf').toggleClass('open')
+})
 
-  // BOTÕES SIDEBAR
-  $('#main-menu-toggle').on('click', function () {
-    $('.content-sf, .navbar').toggleClass('active')
-    $('.sidebar-sf').toggleClass('open')
-  })
-
-  $('#resumed-menu-toggle').on('click', function () {
-    $('.content-sf , .navbar').toggleClass('active-resumed')
-    $('#main-menu-toggle').toggleClass('hide')
-    $('.sidebar-sf').toggleClass('resumed')
-  })
-})()
+$('#resumed-menu-toggle').on('click', function () {
+  $('.content-sf , .navbar').toggleClass('active-resumed')
+  $('#main-menu-toggle').toggleClass('hide')
+  $('.sidebar-sf').toggleClass('resumed')
+})
